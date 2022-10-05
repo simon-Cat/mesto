1f08a9cb51f7fbc582ee92302252f4b8902256d5
Bekir Murtazaev <BekirMurtazaev@yandex.ru>
2022-10-05 18:59:13 +0300

    fix: improve index.html, index.js, block button

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ let profile = document.querySelector('.profile'),
   profile_edit_button = profile.querySelector('.button_type_edit'),
   popup = document.querySelector('.popup'),
   popup_save_button = popup.querySelector('.button_type_submit'),
-  popup_close_button = popup.querySelector('.button_type_close');
+  popup_close_button = popup.querySelector('.button_type_close'),
+  place_like_buttons = document.querySelectorAll('.button_type_like');
 
 profile_edit_button.addEventListener('click', showHidenPopup);
 popup_close_button.addEventListener('click', showHidenPopup);
 popup_save_button.addEventListener('click', saveProfileChanges);
+
+for (let i = 0; i < place_like_buttons.length; i++) {
+  place_like_buttons[i].addEventListener('click', function () {
+    place_like_buttons[i].classList.toggle('button_active');
+  });
+}
 
 function showHidenPopup(evt) {
   evt.preventDefault();
@@ -28,4 +35,6 @@ function saveProfileChanges(evt) {
 
   profile_title.textContent = input_name;
   profile_description.textContent = input_post;
+
+  popup.classList.remove('popup_opened');
 }
