2655408a40d9c7dc8cdfdd844a748be84cc90663
Bekir Murtazaev <BekirMurtazaev@yandex.ru>
2022-10-05 17:11:11 +0300

    fix: improve html tags

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-let profile = document.querySelector(".profile"),
-  profile_title = profile.querySelector(".profile__title"),
-  profile_description = profile.querySelector(".profile__description"),
-  profile_edit_button = profile.querySelector(".button_type_edit"),
-  popup = document.querySelector(".popup"),
-  popup_save_button = popup.querySelector(".button_type_submit"),
-  popup_close_button = popup.querySelector(".button_type_close");
+let profile = document.querySelector('.profile'),
+  profile_title = profile.querySelector('.profile__title'),
+  profile_description = profile.querySelector('.profile__description'),
+  profile_edit_button = profile.querySelector('.button_type_edit'),
+  popup = document.querySelector('.popup'),
+  popup_save_button = popup.querySelector('.button_type_submit'),
+  popup_close_button = popup.querySelector('.button_type_close');
 
-profile_edit_button.addEventListener("click", showHidenPopup);
-popup_close_button.addEventListener("click", showHidenPopup);
-popup_save_button.addEventListener("click", saveProfileChanges);
+profile_edit_button.addEventListener('click', showHidenPopup);
+popup_close_button.addEventListener('click', showHidenPopup);
+popup_save_button.addEventListener('click', saveProfileChanges);
 
 function showHidenPopup(evt) {
   evt.preventDefault();
-  
-  if (!popup.classList.contains("popup_opened")) {
-    popup.classList.add("popup_opened");
+
+  if (!popup.classList.contains('popup_opened')) {
+    popup.classList.add('popup_opened');
   } else {
-    popup.classList.remove("popup_opened");
+    popup.classList.remove('popup_opened');
   }
 }
 
 function saveProfileChanges(evt) {
   evt.preventDefault();
 
-  let input_name = popup.querySelector(".input_type_name").value,
-    input_post = popup.querySelector(".input_type_post").value;
+  let input_name = popup.querySelector('.input_type_name').value,
+    input_post = popup.querySelector('.input_type_post').value;
 
   profile_title.textContent = input_name;
   profile_description.textContent = input_post;
