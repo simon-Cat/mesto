0dcb4cdc5db39887c508bc502ebf0ea51d8d8dda
Bekir Murtazaev <BekirMurtazaev@yandex.ru>
2022-10-11 21:54:14 +0300

    feat: add script for initial load 6 place card

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,50 @@
+// 6 карточек при загрузке страницы
+const initialCards = [
+  { name: 'Абхазия', link: '../images/abkhazia.jpg' },
+  { name: 'Краснодар', link: '../images/krasnodar.jpg' },
+  { name: 'Москва', link: '../images/moscow.jpg' },
+  { name: 'Новороссийск', link: '../images/novorossysk.jpg' },
+  { name: 'Сочи', link: '../images/sohi.jpg' },
+  { name: 'Санкт-Петербург', link: '../images/st-peterburg.jpg' },
+];
+
+// блок places__list
+const placesList = document.querySelector('.places__list');
+
+function addInitialPlaceCard() {
+  initialCards.forEach((item) => {
+    // template блока place
+    const placeTemplate = document
+      .querySelector('#place')
+      .content.cloneNode(true);
+
+    const placeCard = placeTemplate.querySelector('.place');
+
+    // элемент title блока place
+    const placeImage = placeCard.querySelector('.place__image');
+
+    // элемент img блока place
+    const placeTitle = placeCard.querySelector('.place__title');
+    // атрибуты src и alt для элемента place__image
+    placeImage.setAttribute('alt', item.name);
+    placeImage.setAttribute('src', item.link);
+
+    // текст для элемента place__title
+    placeTitle.textContent = item.name;
+
+    // элемент списка li
+    const listItem = document.createElement('li');
+
+    // Добавление блока place в элемент списка li
+    listItem.append(placeCard);
+
+    // Добавление элемента списка li с блоком place в блок places__list
+    placesList.append(listItem);
+  });
+}
+
+addInitialPlaceCard();
+
 // блок profile
 let profile = document.querySelector('.profile'),
   // элемент "title" в блоке profile
