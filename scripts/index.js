4d6a9767d29191b17ac1f9fbea85932db12f3cbf
simon-Cat <110557132+simon-Cat@users.noreply.github.com>
2022-10-14 17:38:50 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 // 6 карточек при загрузке страницы
 const initialCards = [
-  { name: 'Абхазия', link: '../images/abkhazia.jpg' },
-  { name: 'Краснодар', link: '../images/krasnodar.jpg' },
+  { name: 'Абхазия', link: './images/abkhazia.jpg' },
+  { name: 'Краснодар', link: './images/krasnodar.jpg' },
   { name: 'Москва', link: '../images/moscow.jpg' },
   { name: 'Новороссийск', link: '../images/novorossysk.jpg' },
   { name: 'Сочи', link: '../images/sohi.jpg' },
